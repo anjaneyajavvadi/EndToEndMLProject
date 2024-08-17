de9851d5762048f4df3a87659329595f6cd6cb96
anjaneyajavvadi <anjaneyajavvadi@gmail.com>
2024-08-17 18:46:07 +0530

    Stop tracking venv directory

--- src/logger.py
+++ src/logger.py
@@ -2,20 +2,23 @@ import logging
 import os
 from datetime import datetime
 
+# Define log file path
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path = os.path.join(os.getcwd(), "logs")
 os.makedirs(logs_path, exist_ok=True)
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
+# Clear any existing handlers associated with the root logger
+for handler in logging.root.handlers[:]:
+    logging.root.removeHandler(handler)
+
+# Configure logging to only log to a file
 logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    filename=LOG_FILE_PATH,
+    filemode='w',  # 'w' for overwriting the log file every time, 'a' for appending
 )
 
-if __name__ == "__main__":
-    try:
-        a=1/0
-    except:
-        logging.info("DivideByZero")
-        raise CustomException
+# Example logging
+logging.info("This log will only be written to the log file.")
